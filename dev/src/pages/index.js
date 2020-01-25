37d833d7d2c17c94611e37ce47bc8a1529d176b1
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2020-01-25 22:34:59 +0100

    Add Gatsby.js

---
+++ dev/src/pages/index.js
@@ -0,0 +1,190 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { RichText } from "prismic-reactjs";
+import { graphql, Link } from "gatsby";
+import styled from "@emotion/styled";
+import colors from "styles/colors";
+import dimensions from "styles/dimensions";
+import Button from "components/_ui/Button";
+import About from "components/About";
+import Layout from "components/Layout";
+import ProjectCard from "components/ProjectCard";
+
+const Hero = styled("div")`
+    padding-top: 2.5em;
+    padding-bottom: 3em;
+    margin-bottom: 6em;
+    max-width: 830px;
+
+    @media(max-width:${dimensions.maxwidthMobile}px) {
+       margin-bottom: 3em;
+    }
+
+    h1 {
+        margin-bottom: 1em;
+
+        a {
+            text-decoration: none;
+            transition: all 100ms ease-in-out;
+
+            &:nth-of-type(1) { color: ${colors.blue500}; }
+            &:nth-of-type(2) { color: ${colors.orange500}; }
+            &:nth-of-type(3) { color: ${colors.purple500}; }
+            &:nth-of-type(4) { color: ${colors.green500}; }
+            &:nth-of-type(5) { color: ${colors.teal500}; }
+
+            &:hover {
+                cursor: pointer;
+                transition: all 100ms ease-in-out;
+
+                &:nth-of-type(1) { color: ${colors.blue600};    background-color: ${colors.blue200};}
+                &:nth-of-type(2) { color: ${colors.orange600};  background-color: ${colors.orange200};}
+                &:nth-of-type(3) { color: ${colors.purple600};  background-color: ${colors.purple200};}
+                &:nth-of-type(4) { color: ${colors.green600};   background-color: ${colors.green200};}
+                &:nth-of-type(5) { color: ${colors.teal600};    background-color: ${colors.teal200};}
+
+            }
+        }
+    }
+`
+
+const Section = styled("div")`
+    margin-bottom: 10em;
+    display: flex;
+    flex-direction: column;
+
+    @media(max-width:${dimensions.maxwidthTablet}px) {
+        margin-bottom: 4em;
+    }
+
+    &:last-of-type {
+        margin-bottom: 0;
+    }
+`
+
+const WorkAction = styled(Link)`
+    font-weight: 600;
+    text-decoration: none;
+    color: currentColor;
+    transition: all 150ms ease-in-out;
+    margin-left: auto;
+
+    @media(max-width:${dimensions.maxwidthTablet}px) {
+       margin: 0 auto;
+    }
+
+    span {
+        margin-left: 1em;
+        transform: translateX(-8px);
+        display: inline-block;
+        transition: transform 400ms ease-in-out;
+    }
+
+    &:hover {
+        color: ${colors.blue500};
+        transition: all 150ms ease-in-out;
+
+        span {
+            transform: translateX(0px);
+            opacity: 1;
+            transition: transform 150ms ease-in-out;
+        }
+    }
+`
+
+const RenderBody = ({ home, meta }) => (
+    <>
+        <Helmet
+            title={meta.title}
+            titleTemplate={`%s | ${meta.title}`}
+            meta={[
+                {
+                    name: `description`,
+                    content: meta.description,
+                },
+                {
+                    property: `og:title`,
+                    content: meta.title,
+                },
+                {
+                    property: `og:description`,
+                    content: meta.description,
+                },
+                {
+                    property: `og:type`,
+                    content: `website`,
+                },
+                {
+                    name: `twitter:card`,
+                    content: `summary`,
+                },
+                {
+                    name: `twitter:creator`,
+                    content: meta.author,
+                },
+                {
+                    name: `twitter:title`,
+                    content: meta.title,
+                },
+                {
+                    name: `twitter:description`,
+                    content: meta.description,
+                },
+            ].concat(meta)}
+        />
+        <Hero>
+            <>
+                {RichText.render(home.hero_title)}
+            </>
+            <a href={home.hero_button_link.url}
+               target="_blank" rel="noopener noreferrer">
+                <Button>
+                    {RichText.render(home.hero_button_text)}
+                </Button>
+            </a>
+        </Hero>
+
+    </>
+);
+
+export default ({ data }) => {
+    //Required check for no data being returned
+    const doc = data.prismic.allHomes.edges.slice(0, 1).pop();
+    const meta = data.site.siteMetadata;
+
+    if (!doc) return null;
+
+    return (
+        <Layout>
+            <RenderBody home={doc.node} meta={meta}/>
+        </Layout>
+    )
+}
+
+RenderBody.propTypes = {
+    home: PropTypes.object.isRequired,
+    meta: PropTypes.object.isRequired,
+};
+
+export const query = graphql`
+    {
+        prismic {
+            allHomes {
+                edges {
+                    node {
+                        name
+                        text
+                    }
+                }
+            }
+        }
+        site {
+            siteMetadata {
+                title
+                description
+                author
+            }
+        }
+    }
+`
