c7cdf44bfda13fc53f1cc366eb42cb9f538333c1
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2024-02-22 16:57:34 +0100

    add ga4
    Signed-off-by: Sebastian Frederik Jacobsen <hello@hejsfj.com>

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,20 +4,22 @@
 
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/greet/[name].tsx";
-import * as $4 from "./routes/imprint.tsx";
-import * as $5 from "./routes/index.tsx";
+import * as $2 from "./routes/_middleware.ts";
+import * as $3 from "./routes/api/joke.ts";
+import * as $4 from "./routes/greet/[name].tsx";
+import * as $5 from "./routes/imprint.tsx";
+import * as $6 from "./routes/index.tsx";
 import * as $$0 from "./islands/Project.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/greet/[name].tsx": $3,
-    "./routes/imprint.tsx": $4,
-    "./routes/index.tsx": $5,
+    "./routes/_middleware.ts": $2,
+    "./routes/api/joke.ts": $3,
+    "./routes/greet/[name].tsx": $4,
+    "./routes/imprint.tsx": $5,
+    "./routes/index.tsx": $6,
   },
   islands: {
     "./islands/Project.tsx": $$0,
