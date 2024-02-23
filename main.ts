5e8cd421128fb80851295e9b05da5ffc71ede852
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2024-02-23 10:01:07 +0100

    Add GA4 plugin
    Signed-off-by: Sebastian Frederik Jacobsen <hello@hejsfj.com>

--- main.ts
+++ main.ts
@@ -5,9 +5,10 @@
 /// <reference lib="deno.ns" />
 
 import "$std/dotenv/load.ts";
+import { ga4Plugin } from "https://deno.land/x/fresh_ga4@0.0.4/mod.ts";
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
-await start(manifest, config);
+await start(manifest, config, { plugins: [ga4Plugin()] });
