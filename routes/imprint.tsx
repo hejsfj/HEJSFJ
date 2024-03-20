d2be49000945d3f0a996d9d6c2f9449ba471b0be
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2024-03-20 14:43:23 +0100

    Add privavy notice
    Signed-off-by: Sebastian Frederik Jacobsen <hello@hejsfj.com>

--- routes/imprint.tsx
+++ routes/imprint.tsx
@@ -37,6 +37,67 @@ export default function Imprint() {
                       https://www.verbraucher-schlichter.de). Quelle:
                       https://www.e-recht24.de
                     </p>
+                    <p>
+                      Data Privacy
+                    </p>
+                    <p>
+                      The following notes provide a simple overview of what happens to your personal data when you visit our website or use our app. Personal data is all data with which you can be personally identified. For detailed information on data protection, please refer to our privacy policy listed below this text.
+                      1. Introduction
+                      This Privacy Policy (the “Policy”) describes how SFJ Capital UG. (“SFJ” or “we/us/our”) collects, uses, stores, shares and protects the personal information derived from the use made by the user (the “User”) both of the platform accessible via the domain https://hejsfj.com (the “Website”) and our Applications (Fred & Sunny Days Ahead) (the “App”), and through the services that are specifically offered to you via the Website or the App (referred to indistinctly as the “Services”).
+                      If you have any concern or query in connection with the Policy, you can consult us at hello@hejsfj.com.
+                      2. Who is the data controller of your personal data?
+                      The personal data you provide in the User registration process, along with any data submitted during the provision of the services or that SFJ may obtain through the Website and/or the App, will be processed by SFJ Capital UG UG located at Dhauner Straße 42, 67067 Ludwigshafen am Rhein (Deutschland) with HBR number 67315 and whose contact email address is hello@hejsfj.com.
+                      This Policy governs the processing of personal data. All the terms used in the Policy will have the meaning established in the Terms and Conditions of Use (https://hejsfj.com/imprint) unless they are expressly defined in this Privacy Policy.
+                      3. For what purpose do we process your data?
+                      The personal data will be collected and processed in the context of the Services offered through the Website and/or the App for the following purposes:
+                      (a) To manage User registration and to be able to control the development and compliance with the contractual relationship with you, in accordance with the Terms and Conditions of Use. In particular, SFJ will process the following categories of data for the purposes that are specified below:
+
+
+                      (b) To respond to queries that may be submitted by Users in connection with the App and the Website or the Services
+                      (c) To respond to law enforcement requests and as required by applicable law, court order, or governmental regulations.
+                      (d) For any other business purpose stated when collecting your personal data or as otherwise set forth in applicable data privacy laws.
+                      Furthermore, and only with the consent of the User in each case, we will use their data for the following additional purposes:
+                      (a) To better know their preferences through evaluation of their profile, using the data to improve the use of the Website and/or the App, prevent fraud and improve the service.
+                      (b) If so requested, to send the User advertising or commercial information, including by electronic means, about SFJ’s products and services that may be of interest to them.
+                      (c) To process their participation in competitions and specific promotions.
+                      In addition, the User’s personal data will be processed when SFJ has to fulfill a legal obligation under tax and anti-money laundering laws.
+                      Whenever you interact with our App, Website, or Services, we automatically collect information, such as: log data (e.g., access times, hardware, software information), usage information, browsing history, browser information, screen recording of Service interaction and usage, location data (e.g., IP address, zipcode), device identification, “cookie” information, page view statistics, the type of browser and/or device you’re using to access our Services, and the page or feature you requested.
+                      “Cookies” are identifiers we transfer to your browser or device that allow us to recognize your browser or device and tell us how and when pages and features in our Services are visited and by how many people. You may be able to change the preferences on your browser or device to prevent or limit your device’s acceptance of cookies, but this may prevent you from taking advantage of some of our features. For more information on our use of Cookies, please see our Cookies Policy.
+                      Our service providers may also transmit cookies to your browser or device in certain situations, such as when you click on a link to a third party website or service. This Policy does not cover the use of cookies by any third parties, and we are not responsible for their privacy policies and practices. Please be aware that cookies placed by third parties may continue to track your activities online even after you have left our Services, and those third parties may not honor “Do Not Track” requests you have set using your browser or device.
+                      For example, Google, Inc. (“Google”) uses cookies in connection with its Google Analytics services. Google’s ability to use and share information collected by Google Analytics about your visits to the Services is subject to the Google Analytics Terms of Use and the Google Privacy Policy. You have the option to opt out of Google’s use of cookies by visiting the Google advertising opt-out page at www.google.com/privacy_ads.html or the Google Analytics Opt-out Browser Add-on at https://tools.google.com/dlpage/gaoptout/.
+                      4. What is the legitimate interest for processing the data?
+                      The legal basis for processing the data is the performance of the Terms and Conditions of Use of the Services, where the data processing is strictly necessary to be able to use the Website and the App and, therefore, to allow Users to benefit from the services made available to them.
+                      With respect to additional purposes, these are legitimized by the consent given by the User, which may be revoked at any time without affecting the provision of the Services. Specifically, Users may withdraw their consent at any time by contacting us at hello@hejsfj.com.
+                      5. How long will we keep your personal data?
+                      SFJ will keep the personal data of Users while they remain registered customers or users of the Website and/or the App, and the personal data will be retained after the termination of the contractual relationship, duly blocked, for the time necessary to answer for any liabilities that may be derived from the relationship. In some cases we retain personal data for longer, if doing so is necessary to comply with our legal obligations, resolve disputes or collect fees owed, or is otherwise permitted or required by applicable law, rule or regulation.
+                      In particular, the Personal Data required for responding to queries made and delivering electronic marketing communications will be processed for as long as the User does not revoke the consent given and, once that consent has been withdrawn, for the time necessary to answer for any liabilities that may have arisen. In any case, when the personal data is not required for the purpose for which it was obtained and until it is not finally canceled, it will be retained in a blocked form.
+                      As stated in the Terms of Use, we do not knowingly collect personal information from anyone under the age of 18; if you are an individual under 18, please do not attempt to register, use our services or send us your personal information. If we learn that we have collected personal information from anyone under the age of 18, we will delete that information as soon as possible. If you believe that someone younger than 18 years has given us his or her personal information, please contact us through hello@hejsfj.com.
+
+                      6. Who will your data be sent to?
+                      SFJ may communicate the personal data of Users when so required by law, for defending its legitimate rights and interests, or when necessary in accordance with the services contracted by Users. They may also be accessed by third party service providers and computer and technical support services, some of which are detailed below.
+                      6.1 Passing on of data for the fulfilment of the services
+                      Our systems, data and services are hosted on the Google Cloud Platform ("GCP"), whose servers are located at "Europe-west3" and "Europe-west1. Your data may be transferred internationally to servers in the United States. This international transfer of personal data is covered by the EU-USA Privacy Shield Agreement and therefore provides adequate safeguards for the protection of your personal data. More information is available at the following link: https://www.privacyshield.gov/participant?id=a2zt000000001L5AAI&status=Active.
+                      To conclude, we inform you that SFJ relies on other companies to provide certain services related to the Website and/or the App, like billing or payment of Services through our platform. More information on each company is available at the following links:
+                      Google Analytics: https://policies.google.com/privacy/update
+                      Supabase: https://supabase.com/privacy
+                      Google Analytics (https://policies.google.com/privacy/update)
+                      For the purpose of designing our pages to meet your needs and continuously optimizing them, we use Google Analytics, a web analysis service of Google Inc. ("Google"). Google Analytics uses so-called "cookies", text files which are stored on your computer and which enable an analysis of your use of the website. In this context, pseudonymised user profiles are created and cookies are used. The information generated by the cookie about your use of this website such as
+                      Browser type/version, the operating system used,
+                      Referrer URL (the previously visited page),
+                      Host name of the accessing computer (IP address),
+                      Time of the server request
+                      On behalf of the operator of this website, Google will use this information to evaluate your use of the website, to compile reports on the website activities and to provide further services to the website operator in connection with the use of the website and the internet. The IP address transmitted by your browser within the framework of Google Analytics is not combined with other data from Google. You may refuse the use of cookies by selecting the appropriate settings on your browser, however please note that if you do this you may not be able to use the full functionality of this website. You can also prevent the collection of data generated by the cookie and related to your use of the website (including your IP address) to Google and the processing of this data by Google by downloading and installing the browser plugin available under the following link: https://tools.google.com/dlpage/gaoptout?hl=de.
+                      As an alternative to the browser add-on, especially in the case of browsers on mobile devices, you can also prevent Google Analytics from recording the data by clicking on this link. An opt-out cookie is set to prevent future collection of your information when you visit this website. The opt-out cookie is valid only in this browser and only for our website and is stored on your device. If you delete the cookies in this browser, you will need to set the opt-out cookie again. You can find further information on data protection in connection with Google Analytics on the Google Analytics website.
+                      7. What are your rights as the data subject?
+
+
+                      The exercise of all these rights is free.
+                      Contact channel
+                      To exercise your rights, please send an e-mail to hello@hejsfj.com.
+                      8. Information updating
+                      The User may update the information held by SFJ through the settings available in their account, or by writing to hello@hejsfj.com. We remind you that in order for SFJ to adequately provide the Services, the User must keep their information up to date, and accordingly, agrees to regularly check the information provided to ensure it is accurate.
+                      You can ask us to stop sending you marketing emails or communications at any time. To this end, you can change the communications preferences in the settings section of your account, or contact us at hello@hejsfj.com. However, this will not prevent us from sending emails and other communications related to the Services, since these are necessary to comply with the contractual relationship between us and the User.
+                    </p>
                   </div>
                   <div>
                   </div>
