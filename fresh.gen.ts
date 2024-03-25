ebc4710cb108a4524dbf3f4c3c91567dd3fc605a
Sebastian Frederik Jacobsen <hello@hejsfj.com>
2024-03-25 10:27:45 +0100

    Create separate privacy page
    Signed-off-by: Sebastian Frederik Jacobsen <hello@hejsfj.com>

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,6 +6,7 @@ import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $imprint from "./routes/imprint.tsx";
 import * as $index from "./routes/index.tsx";
+import * as $privacy from "./routes/privacy.tsx";
 import * as $Project from "./islands/Project.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
@@ -15,6 +16,7 @@ const manifest = {
     "./routes/_app.tsx": $_app,
     "./routes/imprint.tsx": $imprint,
     "./routes/index.tsx": $index,
+    "./routes/privacy.tsx": $privacy,
   },
   islands: {
     "./islands/Project.tsx": $Project,
